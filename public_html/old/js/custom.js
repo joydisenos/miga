f014cce1a9d8e038d0b68bc0a8009a00160c4211
Joseph <joydisenos@gmail.com>
2018-06-20 14:04:59 -0400

    Protocolo HTTPS

--- public_html/old/js/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	// SCRIPT MODAL OPEN SOLO
-	$(window).load(function(){
-		setTimeout(function(){
-			$('#myModal').modal('show');
-		}, 3000000);
-	});        
-});
-        
